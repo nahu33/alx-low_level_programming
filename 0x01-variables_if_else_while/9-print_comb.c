16b4ba8c9d007e5ee5d8e9e17fb6889bf573a36d
nahu33 <nahu33@gmail.com>
2023-03-17 12:15:12 -0700

    nahu

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,15 @@ int main(void)
 {
 	int num;
 
-	for (num=0;num<=9;num++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar((num%10)+'0');
-		if (num==9)
+		putchar((num % 10) + '0');
+
+		if (num == 9)
 			continue;
 		
 		putchar(',');
+		
 		putchar(' ');
 	}
 
@@ -23,4 +25,3 @@ int main(void)
 
 	return (0);
 }
-
