7e6e8cc828dbca43fca55f0a77c8c61b75ff8672
nahu33 <nahu33@gmail.com>
2023-03-21 08:52:10 -0700

    nahu

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,29 +1,20 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
 
+void print_alphabet(void)
+{
+	char c = 'a';
 
-        /**
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
 
-         * print_alphabet - print all alphabet in lowercase
+	}
+	_putchar('\n');
 
-         */
-
-
-
-        void print_alphabet(void)
-
-        {
-
-                char letter;
-
-
-
-                for (letter = 'a'; letter <= 'z'; letter++)
-
-                        _putchar(letter);
-
-
-
-                _putchar('\n');
-
-        }
+}
